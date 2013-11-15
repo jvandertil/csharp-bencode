7c404ddcc9841d4045b66d46998be07dbac4675b
Jos van der Til <jos@vandertil.eu>
2013-11-15 14:15:17 +0100

    Added example program to show usage

---
+++ BEncodeLib.Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BEncodeLib.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TorrentFile torrent;
+
+            using (var stream = new FileStream("ubuntu-13.10-desktop-amd64.iso.torrent", FileMode.Open))
+            using (var decoded = new TorrentBDecoder(stream, Encoding.UTF8))
+            {
+                var torrentAsDictionary = decoded.Decode() as Dictionary<object, object>;
+
+                torrent = new TorrentFile(torrentAsDictionary, decoded.GetInfoHash());
+            }
+
+            if (torrent.IsMultiAnnounce)
+            {
+                // Trackers are grouped.
+                foreach (var trackerList in torrent.AnnounceList)
+                {
+                    foreach(var tracker in trackerList)
+                        Console.WriteLine("Tracker: " + tracker);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tracker: " + torrent.Announce);
+            }
+        }
+    }
+}
